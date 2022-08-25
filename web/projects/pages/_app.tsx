4dafd5a04060691c3e24257c50d466934ea6ea81
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2022-08-26 08:38:39 +0900

    fix: ServiceWorkerの読み込みエラー
    パス指定が相対指定だったので、
    直接下位パスにアクセスすると読み込みエラーが発生していた。

--- web/projects/pages/_app.tsx
+++ web/projects/pages/_app.tsx
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       try {
-        navigator.serviceWorker.register('sw.js');
+        navigator.serviceWorker.register('/sw.js');
         console.log('SW registered');
       } catch (e) {
         console.log('SW faild');
