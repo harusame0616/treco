ce8e1ff6e181ae3b932c8b6f47cc0f524dd35869
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2022-09-10 21:58:12 +0000

    style: リストアイテムカードのフォントを調整

--- web/projects/components/case/list-item-card.tsx
+++ web/projects/components/case/list-item-card.tsx
@@ -11,7 +11,7 @@ const ListItemCard = (prop: Prop) => {
   return (
     <BaseCard onClick={prop.onClick}>
       <Box
-        fontSize="1.5rem"
+        fontSize="20px"
         sx={{ minHeight: '38px' }}
         display="flex"
         alignItems="center"
