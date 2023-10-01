d072af7ef9c4cc0138cbbbb60768d42749d19b89
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-01 22:24:02 +0900

    トップページ仮実装

--- treco-web/src/app/(public)/page.tsx
+++ treco-web/src/app/(public)/page.tsx
@@ -1,7 +1,34 @@
 import Image from 'next/image';
+import SplashImage from './splash.svg';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col w-full items-center px-4 py-14 gap-10">
+      <Image
+        priority
+        src={SplashImage}
+        alt="TRECo BESIDE YOUR WORKOUT"
+        width="500"
+      />
+      <p className="text-[min(5vw,28px)] text-center flex flex-col gap-2">
+        <span className="block">トレーニングをもっと楽しくする</span>
+        <span className="block">シンプルなトレーニング記録サービス</span>
+      </p>
+      <div className="flex flex-col gap-2">
+        <Button>Google でログイン</Button>
+        <Button>Twitter でログイン</Button>
+        <Button>Facebook でログイン</Button>
+        <Button variant="ghost" className="text-primary">
+          登録せず開始する
+        </Button>
+      </div>
+      <div className="text-xs">
+        <Link href="/policies/term-of-service">利用規約</Link>、
+        <Link href="/policies/privacy-policy">プライバシーポリシー</Link>
+        に同意の上ご利用ください。
+      </div>
+    </main>
   );
 }
--- treco-web/src/app/layout.tsx
+++ treco-web/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { M_PLUS_Rounded_1c } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const baseFont = M_PLUS_Rounded_1c({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export const metadata: Metadata = {
   title: 'TRECo - BESIDE YOUR WORKOUT',
@@ -17,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={`${baseFont.className} dark`}>{children}</body>
     </html>
   );
 }
