62e0cb3ab850b1943e0eeb7f62a195a2ed316d84
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-07 17:50:57 +0900

    カレンダー実装

---
+++ treco-web/src/app/(header)/(auth)/home/_component/calendar-month.tsx
@@ -0,0 +1,68 @@
+'use client';
+import dayjs from 'dayjs';
+import { memo } from 'react';
+import { Day } from './day';
+
+type Props = {
+  year: number;
+  month: number;
+  selectDate: Date;
+  onSelectDate: (date: Date) => void;
+};
+
+export function CalendarMonth({
+  year,
+  month,
+  onSelectDate,
+  selectDate,
+}: Props) {
+  const daysOfWeek = [
+    {
+      label: '日',
+      color: 'text-red-500',
+    },
+    { label: '月' },
+    { label: '火' },
+    { label: '水' },
+    { label: '木' },
+    { label: '金' },
+    { label: '土', color: 'text-blue-500' },
+  ];
+
+  const firstDate = dayjs(`${year}-${month}-01`).startOf('month');
+  const dayOfFirstDate = firstDate.day();
+  const calenderStart = firstDate.subtract(dayOfFirstDate, 'day');
+  const days = new Array(7 * 6)
+    .fill(0)
+    .map((_, index) => calenderStart.add(index, 'day'));
+
+  return (
+    <div>
+      {month}
+      <div className="p-4 bg-muted">
+        <div className="grid grid-cols-7">
+          {daysOfWeek.map((day) => (
+            <div
+              key={day.label}
+              className={`${day.color} text-xs text-center mb-1`}
+            >
+              {day.label}
+            </div>
+          ))}
+          {days.map((day) => {
+            return (
+              <Day
+                key={day.format('YYYY-MM-DD')}
+                date={day.toDate()}
+                mute={!day.isSame(firstDate, 'month')}
+                highlight={day.isSame(selectDate, 'day')}
+                active={dayjs().isSame(day, 'day')}
+                onSelectDate={onSelectDate}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
