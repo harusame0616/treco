24a03d1e8910b8121332200048bf00eb60901b42
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-16 00:14:37 +0900

    getSignedInTraineeId が常に値を返すように修正

--- treco-web/src/lib/trainee/index.ts
+++ treco-web/src/lib/trainee/index.ts
@@ -4,5 +4,10 @@ export const SIGNED_IN_TRAINEE_ID_COOKIE_NAME =
   'SIGNED_IN_TRAINEE_ID_COOKIE_NAME';
 
 export async function getSignedInTraineeId() {
-  return cookies().get(SIGNED_IN_TRAINEE_ID_COOKIE_NAME)?.value;
+  const traineeId = cookies().get(SIGNED_IN_TRAINEE_ID_COOKIE_NAME)?.value;
+  if (!traineeId) {
+    throw new Error('No signed in trainee Id');
+  }
+
+  return traineeId;
 }
