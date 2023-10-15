59603fa7bb5bec072e374952d6328acb5db5a8b0
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-16 00:23:35 +0900

    firebase の client 設定を環境変数から取得するように修正

--- treco-web/src/lib/firebase/client.ts
+++ treco-web/src/lib/firebase/client.ts
@@ -4,7 +4,17 @@ import { connectAuthEmulator, getAuth } from 'firebase/auth';
 
 import firebaseConfig from './settings/firebase-config.json';
 
-const app = initializeApp(firebaseConfig);
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+};
+
+const app = initializeApp(firebaseConfig || config);
 
 declare global {
   // eslint-disable-next-line no-var
