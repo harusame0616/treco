56797cb0e83789ce093141eec828b3ce6c2a10e7
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-17 23:01:55 +0900

    pino インストール

--- treco-web/src/app/e/page.tsx
+++ treco-web/src/app/e/page.tsx
@@ -1,11 +1,17 @@
 import { logger } from '@/lib/logger';
 
 export default function ErrorSamplePage() {
-  logger.error(
-    {
-      e: new Error('sample error'),
-    },
-    'sample error',
-  );
+  logger.trace({ msg: 'trace' });
+  logger.debug({ msg: 'debug' });
+  logger.info({ msg: 'info' });
+  logger.warn({ msg: 'warn' });
+  logger.error({
+    msg: 'error',
+    e: new Error('sample error'),
+  });
+  logger.fatal({
+    msg: 'error',
+    e: new Error('sample error'),
+  });
   return <div>error</div>;
 }
--- treco-web/src/lib/logger.ts
+++ treco-web/src/lib/logger.ts
@@ -1,4 +1,5 @@
 import pino from 'pino';
+import { logflarePinoVercel } from 'pino-logflare';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
