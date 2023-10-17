b9e4276a4a585790022aa96b54a83b900617f7e4
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-17 23:01:55 +0900

    default 追加

--- treco-web/src/app/e/page.tsx
+++ treco-web/src/app/e/page.tsx
@@ -1,17 +1,17 @@
 import { logger } from '@/lib/logger';
 
 export default function ErrorSamplePage() {
-  logger.trace({ msg: 'trace' });
-  logger.debug({ msg: 'debug' });
-  logger.info({ msg: 'info' });
-  logger.warn({ msg: 'warn' });
+  logger.trace({ level: 'trace' }, 'test');
+  logger.debug({ level: 'debug' }, 'test');
+  logger.info({ level: 'info' }, 'test');
+  logger.warn({ level: 'warn' }, 'test');
   logger.error({
-    msg: 'error',
-    e: new Error('sample error'),
+    level: 'error',
+    err: new Error('sample error'),
   });
   logger.fatal({
-    msg: 'error',
-    e: new Error('sample error'),
+    level: 'fatal',
+    err: new Error('sample error'),
   });
   return <div>error</div>;
 }
