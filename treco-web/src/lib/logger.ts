c23d3c262683aa1bf3dda5ef3047a6e29dd27275
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-17 23:01:55 +0900

    ログテスト

--- treco-web/src/lib/logger.ts
+++ treco-web/src/lib/logger.ts
@@ -1,6 +1,5 @@
 import pino from 'pino';
-import { logflarePinoVercel } from 'pino-logflare';
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
+  level: process.env.LOG_LEVEL || 'trace',
 });
