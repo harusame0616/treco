025e353dee7701f3de99537ddddab6de7a89ce13
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-10-18 08:10:09 +0900

    middleware

---
+++ treco-web/src/middleware.ts
@@ -0,0 +1,17 @@
+import { logger } from './lib/logger';
+
+export default function middleware() {
+  logger.trace({ level: 'trace' }, 'test');
+  logger.debug({ level: 'debug' }, 'test');
+  logger.info({ level: 'info' }, 'test');
+  logger.warn({ level: 'warn' }, 'test');
+  logger.error({
+    level: 'error',
+    err: new Error('sample error'),
+  });
+  logger.fatal({
+    level: 'fatal',
+    err: new Error('sample error'),
+  });
+  console.error('console error');
+}
