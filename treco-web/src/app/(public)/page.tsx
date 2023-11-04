9649d3be9252064774c2c9d09ea392fe875b139b
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-11-04 21:44:36 +0900

    QRコード表示

--- treco-web/src/app/(public)/page.tsx
+++ treco-web/src/app/(public)/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { LoginButtonList } from '../login-button-list';
+import QRCode from './qr.png';
 import SplashImage from './splash.svg';
 
 export default async function Home() {
@@ -23,14 +24,24 @@ export default async function Home() {
         <span className="block">トレーニングをもっと楽しくする</span>
         <span className="block">シンプルなトレーニング記録サービス</span>
       </p>
-      <div className="flex flex-col gap-2">
-        <LoginButtonList />
-      </div>
-      <div className="text-xs">
+      <LoginButtonList />
+      <p className="text-xs">
         <Link href="/policies/term-of-service">利用規約</Link>、
         <Link href="/policies/privacy-policy">プライバシーポリシー</Link>
         に同意の上ご利用ください。
-      </div>
+      </p>
+      <section className="flex flex-col items-center text-xs">
+        <p className="mb-4">
+          本サービスはモバイル端末向けに最適化されています。
+          <br />
+          以下のQRコードからモバイル端末でご利用ください。
+        </p>
+        <Image
+          alt="trec.fit へのリンクの QR Code 画像"
+          className="h-20 w-20"
+          src={QRCode}
+        />
+      </section>
     </main>
   );
 }
