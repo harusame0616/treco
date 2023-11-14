a8c18c979c3a9d627cc78ba627f7d6abb6fe31ae
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2023-11-14 23:07:05 +0900

    Renovate 設定 (#91)
    * renovate の設定ファイル作成
    
    * dependabot 削除
    
    * CI テストに除外パス追加
    
    * テスト対象変更
    
    * コメント削除

--- treco-web/playwright.config.ts
+++ treco-web/playwright.config.ts
@@ -1,23 +1,14 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 
 const defaultUserStorageState = 'playwright/.auth/defaultTrainee.json';
 const setupProjectName = 'setup';
 export default defineConfig({
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Configure projects for major browsers */
   projects: [
     {
       name: setupProjectName,
@@ -31,61 +22,21 @@ export default defineConfig({
         storageState: defaultUserStorageState,
       },
     },
-
     {
-      dependencies: [setupProjectName],
-      name: 'firefox',
+      name: 'Mobile Chrome',
       use: {
-        ...devices['Desktop Firefox'],
+        ...devices['Pixel 5'],
         storageState: defaultUserStorageState,
       },
     },
-
-    {
-      dependencies: [setupProjectName],
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-        storageState: defaultUserStorageState,
-      },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   testDir: './e2e',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
     baseURL: 'http://localhost:3000',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
-  /* Run your local dev server before starting the tests */
   webServer: process.env.CI
     ? {
         command: 'npm run start',
@@ -93,7 +44,5 @@ export default defineConfig({
         url: 'http://127.0.0.1:3000',
       }
     : undefined,
-
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
 });
