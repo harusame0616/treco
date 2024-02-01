3307a89c7e34dc8d63e4ff5aa4d835f4985c3bf9
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-02-01 22:50:40 +0900

    delete-button を共通化

--- treco-web/src/app/(header)/(auth)/home/categories/[categoryId]/events/_components/event-delete.tsx
+++ treco-web/src/app/(header)/(auth)/home/categories/[categoryId]/events/_components/event-delete.tsx
@@ -1,67 +1,29 @@
 'use client';
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
-import { TrashIcon } from '@radix-ui/react-icons';
-import { useState } from 'react';
+import { DeleteButton } from '@/components/delete-button';
 
 import { deleteTrainingEventAction } from '../_actions/delete-training-event.action';
+
+function Description() {
+  return (
+    <>
+      この操作は取り消せません。また、関連する以下のデータも一緒に削除されます。
+      <span className="mt-8 block">・トレーニング記録</span>
+    </>
+  );
+}
 
 type Props = {
   trainingCategoryId: string;
   trainingEventId: string;
 };
-export function EventDelete({ trainingCategoryId, trainingEventId }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isProcessing, setIsProcessing] = useState(false);
-  async function deleteTrainingEvent() {
-    setIsProcessing(true);
-    try {
-      await deleteTrainingEventAction({ trainingCategoryId, trainingEventId });
-    } finally {
-      setIsProcessing(false);
-    }
-    setIsOpen(false);
-  }
-
+export function EventDelete(props: Props) {
   return (
-    <AlertDialog onOpenChange={setIsOpen} open={isOpen}>
-      <AlertDialogTrigger asChild>
-        <Button className="h-full" variant="destructive">
-          <span className="sr-only">削除</span>
-          <TrashIcon aria-hidden="true" className="h-12 w-14" />
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>
-            本当にトレーニング種目を削除しますか？
-          </AlertDialogTitle>
-          <AlertDialogDescription>
-            この操作は取り消せません。また、関連する以下のデータも一緒に削除されます。
-            <span className="mt-8 block">・トレーニング記録</span>
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>やめる</AlertDialogCancel>
-          <AlertDialogAction
-            disabled={isProcessing}
-            onClick={deleteTrainingEvent}
-          >
-            トレーニング種目を削除する
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <DeleteButton
+      action={() => deleteTrainingEventAction(props)}
+      description={<Description />}
+      submitLabel="トレーニング種目を削除する"
+      title="本当にトレーニング種目を削除しますか？"
+    />
   );
 }
