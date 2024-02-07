0a95242b9a4fdcbb3115f77d2d89f3cccfb2a55e
Nemoto Masaharu <57859567+harusame0616@users.noreply.github.com>
2024-02-08 00:59:34 +0900

    top ページの redirect 削除漏れ修正 (#152)
    * トップページを static rendering に変更
    
    * server component の redirect 削除漏れ修正

--- treco-web/src/app/(public)/page.tsx
+++ treco-web/src/app/(public)/page.tsx
@@ -1,7 +1,5 @@
-import { isAuthenticated } from '@/lib/auth/auth';
 import Image from 'next/image';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
 
 import { LoginButtonList } from '../login-button-list';
 import QRCode from './qr.png';
@@ -9,10 +7,6 @@ import { RedirectHome } from './redirect-home';
 import SplashImage from './splash.svg';
 
 export default async function Top() {
-  if (await isAuthenticated()) {
-    redirect('/home');
-  }
-
   return (
     <main className="flex w-full flex-col items-center gap-10 px-4 py-14">
       <Image
